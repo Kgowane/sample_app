c8eec3fa6d76fb70afb3c61b3784f3bbe4c708e2
Kgowane <Kgowane@yahoo.com>
2019-03-28 10:05:19 +0200

    Finish layout and routes

---
+++ app/example_user.rb
@@ -0,0 +1,21 @@
+class User
+  attr_accessor :name, :email
+
+  def initialize(attributes = {})
+    @firstname  = attributes[:firstname]
+    @lastname = attributes[:lastname]
+    @email = attributes[:email]
+  end
+
+  def full_name
+    "#{@firstname} #{@lastname}"
+  end
+
+  def formatted_email
+    full_name " <#{@email}>"
+  end
+
+  def alphabetical_name
+    "#{@lastname}, #{@firstname}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
+
+  get '/help', to: 'static_pages#help', as: 'help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/about', to: 'static_pages#about', as: 'about'
+  get '/contact', to: 'static_pages#contact', as: 'contact'
+  get '/signup', to: 'users#new', as: 'signup'
 end
