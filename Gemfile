941f26e8a4d9d1695dbaf1871f932dd1e49e4822
Kgowane <Kgowane@yahoo.com>
2019-04-01 15:00:09 +0200

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'rails', '~> 5.2.2', '>= 5.2.2.1'
 #Use jquery-rails for javascripts
 gem 'jquery-rails','4.3.1'
 
+gem 'pry', '~> 0.12.2'
+
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,8 +8,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should get new" do
     get signup_path
+     # params: { user: { name: 'Steve Busemi', email: 'steve@busemi.com', password: 'pass1234', password_confirmation: 'pass1234' } }
     assert_response :success
     assert_select "title", "Sign Up | #{@base_title}"
   end
-
 end
