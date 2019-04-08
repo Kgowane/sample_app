381fe9304cfd9902b8624a364a3a536823823832
Kgowane <Kgowane@yahoo.com>
2019-04-08 10:24:59 +0200

    Account activation with letter opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,4 +65,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
   # Use this if developing on localhost.
   # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
+  # letter_opener
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
